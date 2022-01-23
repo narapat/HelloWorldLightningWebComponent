5d4e848d5958c3e474c63012e74f5424622300d3
Narapat <narapat@hotmail.com>
2022-01-24 01:13:27 +0700

    นา

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,6 +1,6 @@
 import { LightningElement } from 'lwc';
 export default class HelloWorld extends LightningElement {
-  greeting = 'Hello World';
+  greeting = 'World';
   changeHandler(event) {
     this.greeting = event.target.value;
   }
